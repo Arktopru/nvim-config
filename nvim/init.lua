12a5e0878a52c6091239218ec76c5c860e547319
Arcady <Arktopru@users.noreply.github.com>
2026-04-09 17:28:03 +1000

    Первая рабочая версия конфига

---
+++ nvim/init.lua
@@ -0,0 +1,93 @@
+-- Declare the path where lazy will clone plugin code
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+-- Check to see if lazy itself has been cloned, if not clone it into the lazy.nvim directory
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+
+-- Add the path to the lazy plugin repositories to the vim runtime path
+vim.opt.rtp:prepend(lazypath)
+
+-- Declare a few options for lazy
+local opts = {
+	change_detection = {
+		-- Don't notify us every time a change is made to the configuration
+		notify = false,
+	},
+	checker = {
+		-- Automatically check for package updates
+		enabled = true,
+		-- Don't spam us with notification every time there is an update available
+		notify = false,
+	},
+}
+
+-- Load the options from the config/options.lua file
+require("config.options")
+-- Load the keymaps from the config/keymaps.lua file
+require("config.keymaps")
+-- Load the auto commands from the config/autocmds.lua file
+-- require("config.autocmds")
+-- Setup lazy, this should always be last
+-- Tell lazy that all plugin specs are found in the plugins directory
+-- Pass it the options we specified above
+require("lazy").setup("plugins", opts)
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+
+vim.filetype.add({
+	extension = {
+		ftl = "html",
+		"css",
+		"freemarker", -- Treat .myhtml files as html
+	},
+})
+
+-- Add a custom extension to be detected as 'html'
+-- Open the current file in the default system browser
+vim.keymap.set("n", "<leader>ob", function()
+	vim.ui.open(vim.fn.expand("%:p"))
+end, { desc = "[O]pen file in system default [B]rowser" })
+
+if vim.g.neovide then
+	vim.o.guifont = "JetBrainsMono Nerd Font Mono:h15"
+
+	-- vim.g.neovide_progress_bar_enabled = true
+	-- vim.g.neovide_progress_bar_height = 5.0
+	-- vim.g.neovide_progress_bar_animation_speed = 200.0
+	-- vim.g.neovide_progress_bar_hide_delay = 0.2
+
+	-- vim.g.neovide_text_gamma = 0.0
+	-- vim.g.neovide_text_contrast = 0.5
+
+	local function save()
+		vim.cmd.write()
+	end
+	local function copy()
+		vim.cmd([[normal! "+y]])
+	end
+	local function paste()
+		vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
+	end
+
+	vim.keymap.set({ "n", "i", "v" }, "<D-s>", save, { desc = "Save" })
+	vim.keymap.set("v", "<D-c>", copy, { silent = true, desc = "Copy" })
+	vim.keymap.set({ "n", "i", "v", "c", "t" }, "<D-v>", paste, { silent = true, desc = "Paste" })
+
+	vim.g.neovide_position_animation_length = 0
+	vim.g.neovide_cursor_animation_length = 0.00
+	vim.g.neovide_cursor_trail_size = 0
+	vim.g.neovide_cursor_animate_in_insert_mode = false
+	vim.g.neovide_cursor_animate_command_line = false
+	vim.g.neovide_scroll_animation_far_lines = 0
+	vim.g.neovide_scroll_animation_length = 0.00
+end
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,90 @@
+-- Set our leader keybinding to space
+-- Anywhere you see <leader> in a keymapping specifies the space key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Remove search highlights after searching
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove search highlights" })
+
+-- Exit Vim's terminal mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- OPTIONAL: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Better window navigation
+vim.keymap.set("n", "<C-h>", ":wincmd h<cr>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", ":wincmd l<cr>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", ":wincmd j<cr>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", ":wincmd k<cr>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "<leader>tc", ":tabnew<cr>", {desc = "[T]ab [C]reat New"})
+vim.keymap.set("n", "<leader>tn", ":tabnext<cr>", {desc = "[T]ab [N]ext"})
+vim.keymap.set("n", "<leader>tp", ":tabprevious<cr>", {desc = "[T]ab [P]revious"})
+
+-- Easily split windows
+vim.keymap.set("n", "<leader>wv", ":vsplit<cr>", { desc = "[W]indow Split [V]ertical" })
+vim.keymap.set("n", "<leader>wh", ":split<cr>", { desc = "[W]indow Split [H]orizontal" })
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left in visual mode" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right in visual mode" })
+
+
+-- Move to previous/next
+vim.keymap.set('n', '<leader>b,', '<Cmd>BufferPrevious<CR>', {desc = "[B]uffer previous [,]"})
+vim.keymap.set('n', '<leader>b.', '<Cmd>BufferNext<CR>', {desc = "[B]uffer next [.]"})
+
+-- Re-order to previous/next
+vim.keymap.set('n', '<leader>b<', '<Cmd>BufferMovePrevious<CR>', {desc = "[B]uffer reorder to previous [<]"})
+vim.keymap.set('n', '<leader>b>', '<Cmd>BufferMoveNext<CR>', {desc = "[B]uffer reorder to next [>]"})
+
+-- Goto buffer in position...
+vim.keymap.set('n', '<leader>b1', '<Cmd>BufferGoto 1<CR>', {desc = "[B]uffer go to [1]"})
+vim.keymap.set('n', '<leader>b2', '<Cmd>BufferGoto 2<CR>', {desc = "[B]uffer go to [2]"})
+vim.keymap.set('n', '<leader>b3', '<Cmd>BufferGoto 3<CR>', {desc = "[B]uffer go to [3]"})
+vim.keymap.set('n', '<leader>b4', '<Cmd>BufferGoto 4<CR>', {desc = "[B]uffer go to [4]"})
+vim.keymap.set('n', '<leader>b5', '<Cmd>BufferGoto 5<CR>', {desc = "[B]uffer go to [5]"})
+vim.keymap.set('n', '<leader>b6', '<Cmd>BufferGoto 6<CR>', {desc = "[B]uffer go to [6]"})
+vim.keymap.set('n', '<leader>b7', '<Cmd>BufferGoto 7<CR>', {desc = "[B]uffer go to [7]"})
+vim.keymap.set('n', '<leader>b8', '<Cmd>BufferGoto 8<CR>', {desc = "[B]uffer go to [8]"})
+vim.keymap.set('n', '<leader>b9', '<Cmd>BufferGoto 9<CR>', {desc = "[B]uffer go to [9]"})
+vim.keymap.set('n', '<leader>b0', '<Cmd>BufferLast<CR>', {desc = "[B]uffer go to [0]"})
+
+-- Pin/unpin buffer
+vim.keymap.set('n', '<leader>bp', '<Cmd>BufferPin<CR>', {desc = "[B]uffer [P]in / unpin buffer"})
+
+-- Goto pinned/unpinned buffer
+--                 :BufferGotoPinned
+--                 :BufferGotoUnpinned
+
+-- Close buffer
+vim.keymap.set('n', '<leader>bc', '<Cmd>BufferClose<CR>', {desc = "[B]uffer [C]lose"})
+
+-- Wipeout buffer
+--                 :BufferWipeout
+
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+
+-- Magic buffer-picking mode
+-- vim.keymap.set('n', '<C-p>',   '<Cmd>BufferPick<CR>', {desc = "[B]uffer []"})
+-- vim.keymap.set('n', '<C-s-p', '<Cmd>BufferPickDelete<CR>', {desc = "[B]uffer []"})
+
+-- Sort automatically by...
+vim.keymap.set('n', '<leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>', {desc = "[B]uffer by [B]uffer number"})
+vim.keymap.set('n', '<leader>bn', '<Cmd>BufferOrderByName<CR>', {desc = "[B]uffer by [N]ame"})
+vim.keymap.set('n', '<leader>bd', '<Cmd>BufferOrderByDirectory<CR>', {desc = "[B]uffer by [D]irectory"})
+vim.keymap.set('n', '<leader>bl', '<Cmd>BufferOrderByLanguage<CR>', {desc = "[B]uffer by [L]nguage"})
+vim.keymap.set('n', '<leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>', {desc = "[B]uffer by [W]indow number"})
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
