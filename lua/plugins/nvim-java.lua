285c12fa04779d2edee336247e39f5b0f5f7a18c
Arcady Zelenin <zarc@japanacar.ru>
2026-04-10 21:50:15 +1000

    Настройки dap

---
+++ lua/plugins/nvim-java.lua
@@ -0,0 +1,55 @@
+return {
+	"nvim-java/nvim-java",
+	config = function()
+		require("java").setup({
+			-- Startup checks
+			checks = {
+				nvim_version = true, -- Check Neovim version
+				nvim_jdtls_conflict = true, -- Check for nvim-jdtls conflict
+			},
+			-- Extensions
+			lombok = {
+				enable = true,
+			},
+
+			java_test = {
+				enable = true,
+			},
+
+			java_debug_adapter = {
+				enable = true,
+			},
+
+			spring_boot_tools = {
+				enable = true,
+			},
+
+			-- JDK installation
+			jdk = {
+				auto_install = true,
+				version = "25",
+			},
+
+			-- Logging
+			log = {
+				use_console = true,
+				use_file = true,
+				level = "info",
+				log_file = vim.fn.stdpath("state") .. "/nvim-java.log",
+				max_lines = 1000,
+				show_location = false,
+			},
+			root_markers = {
+				"settings.gradle",
+				"settings.gradle.kts",
+				"pom.xml",
+				"build.gradle",
+				"mvnw",
+				"gradlew",
+				"build.gradle",
+				"build.gradle.kts",
+			},
+		})
+		vim.lsp.enable("jdtls")
+	end,
+}
