a1ea010a5fed19c311566533d5aae8f1627cc9f6
Arcady Zelenin <zarc@japanacar.ru>
2026-04-16 17:15:56 +1000

    Harpoon настройка ширины окна

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,19 +2,34 @@ local M = {
     "ThePrimeagen/harpoon",
     event = "VeryLazy",
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
     },
     config = function()
         -- Set a vim motion to <Shift>m to mark a file with harpoon
-        vim.keymap.set("n", "<s-m>", "<cmd>lua require('plugins.harpoon').mark_file()<cr>", {desc = "Harpoon Mark File"})
+        vim.keymap.set(
+            "n",
+            "<s-m>",
+            "<cmd>lua require('plugins.harpoon').mark_file()<cr>",
+            { desc = "Harpoon Mark File" }
+        )
         -- Set a vim motion to the tab key to open the harpoon menu to easily navigate frequented files
-        vim.keymap.set("n", "<TAB>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", {desc = "Harpoon Toggle Menu"})
-    end
+        vim.keymap.set(
+            "n",
+            "<TAB>",
+            "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
+            { desc = "Harpoon Toggle Menu" }
+        )
+        require("harpoon").setup({
+            menu = {
+                width = 100, -- Set width to a larger value (default is ~60)
+            },
+        })
+    end,
 }
 
 function M.mark_file()
     require("harpoon.mark").add_file()
-    vim.notify "󱡅  marked file"
+    vim.notify("󱡅  marked file")
 end
 
 return M
